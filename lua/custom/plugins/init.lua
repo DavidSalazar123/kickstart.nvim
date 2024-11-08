13334aa36b82d89aa322eddf68b55814df28652a
David Salazar <davidsalazar200@gmail.com>
2024-11-08 09:41:25 -0500

    trouble plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -29,4 +29,21 @@ return {
       }
     end,
   },
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '[da',
+        '<cmd>Trouble diagnostics toggle focus=true<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '[df',
+        '<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+    },
+  },
 }
