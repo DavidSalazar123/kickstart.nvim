a30f417bb518db84443469fdbd5d1f7de95e2472
David Salazar <davidsalazar200@gmail.com>
2024-12-29 12:02:25 -0500

    Theme

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -73,6 +73,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'debugpy',
       },
     }
 
@@ -110,5 +111,8 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+
+    -- Install python specific config
+    require('dap-python').setup {}
   end,
 }
