2ba72edfb3620d028c14d0d16b0e961e12d24d45
David Salazar <davidsalazar200@gmail.com>
2025-03-30 16:32:28 -0400

    Fix find and replace

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -121,29 +121,32 @@ return {
     },
   },
   {
-    {
-      'nvim-pack/nvim-spectre',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      keys = {
-        {
-          '<leader>fr',
-          function()
-            require('spectre').open()
-          end,
-          desc = 'Open Spectre search panel',
-        },
-        {
-          '<leader>fc',
-          function()
-            require('spectre').close()
-          end,
-          desc = 'Close Spectre search panel',
-        },
+    'nvim-pack/nvim-spectre',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      {
+        '<leader>fr',
+        function()
+          require('spectre').open()
+        end,
+        desc = 'Open Spectre search panel',
       },
-      config = function()
-        require('spectre').setup()
-      end,
+      {
+        '<leader>fc',
+        function()
+          require('spectre').close()
+        end,
+        desc = 'Close Spectre search panel',
+      },
     },
+    config = function()
+      require('spectre').setup {
+        replace_engine = {
+          cmd = 'sed',
+          args = { '-i', '' },
+        },
+      }
+    end,
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
