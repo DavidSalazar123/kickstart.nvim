ccaf24d5ccbd5e7a8ba71115f9d8373a0cdf1b5f
DavidSalazar200 <davidsalazar200@gmail.com>
2026-01-18 10:10:12 -0500

    Add nice brackets animation

--- lua/custom/plugins/snacks-animated-scrolling-off.lua
+++ lua/custom/plugins/snacks-animated-scrolling-off.lua
@@ -1,10 +1,32 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-        scroll = { enabled = false },
-        dashboard = { enabled = false },
+  "folke/snacks.nvim",
+  opts = {
+    scroll = { enabled = false },
+    dashboard = { enabled = false },
+
+    indent = {
+      -- ENABLE the Snacks indent system (required!)
+      enabled = true,
+
+      -- Disable normal/static indent guides
+      indent = {
+        enabled = false,
+      },
+
+      -- Enable ONLY the current scope
+      scope = {
+        enabled = true,
+        only_current = true,
+
+        -- Animated guides for the active block
+        animate = {
+          enabled = true,
+        },
+      },
     },
-    keys = {
-        history = false,
-    }
+  },
+  keys = {
+    history = false,
+  },
 }
+
